6abf1289bd9da3c6ff4961872a612297130da199
Tim Mason <millionsofbillions@gmail.com>
2015-12-06 15:24:26 -0800

    ins integration working and noise added

--- errorStateKF.py
+++ errorStateKF.py
@@ -3,15 +3,20 @@ import transformations as trans
 import matplotlib.pyplot as plt
 
 
-dt = 0.1
+dt = 0.01
 time = np.arange(0,50,dt)
-yaw = np.sin(time/2)
-pitch = yaw
-roll = yaw
+sigma = 0.05
 
-yawRate = np.diff(yaw) / np.diff(time)
-rollRate = np.diff(roll) / np.diff(time)
-pitchRate = np.diff(pitch) / np.diff(time)
+yaw = np.sin(time/2) 
+pitch = 1.1*yaw
+roll = np.sin(time/4)
+
+
+
+
+yawRate = np.diff(yaw) / np.diff(time)  #psi
+pitchRate = np.diff(pitch) / np.diff(time) #phi
+rollRate = np.diff(roll) / np.diff(time)   #theta
 
 yaw = np.delete(yaw,0)
 pitch = np.delete(pitch,0)
@@ -23,18 +28,34 @@ p = rollRate - yawRate * np.sin(pitch)
 q = pitchRate * np.cos(roll) + yawRate * np.sin(roll) * np.cos(pitch)
 r = -pitchRate * np.sin(roll) + yawRate * np.cos(roll) * np.cos(pitch)
 
+p = p + sigma * np.random.randn(len(p))
+q = q + sigma * np.random.randn(len(q))
+r = r + sigma * np.random.randn(len(r))
+
+
+
 quat_ins = trans.quaternion_from_euler(0,0,0)
 
-angles = []
+yawEst = []
+pitchEst = []
+rollEst = []
+
 omegas = []
 for i,t in enumerate(time):
 #integrate ins
     omega = np.array([0,p[i], q[i], r[i]])
     quat_ins = quat_ins + 0.5*dt*trans.quaternion_multiply(quat_ins, omega)
-    angles.append(trans.euler_from_quaternion(quat_ins)[0])
-    omegas.append(p[i])
+    yawEst.append(trans.euler_from_quaternion(quat_ins)[0])
+    pitchEst.append(trans.euler_from_quaternion(quat_ins)[1]) 
+    rollEst.append(trans.euler_from_quaternion(quat_ins)[2])
+
 
 
 plt.plot(time,yaw)
-plt.plot(time,angles)
-#plt.plot(time,omegas)
+plt.plot(time,yawEst)
+
+plt.plot(time,pitch)
+plt.plot(time,pitchEst)
+
+plt.plot(time,roll)
+plt.plot(time,rollEst)
